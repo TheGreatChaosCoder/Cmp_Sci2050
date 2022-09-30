6eaaf0fd4594576eca5b01e1a29088b352e90a91
Connor Johnson <35439715+TheGreatChaosCoder@users.noreply.github.com>
2022-09-29 23:52:59 -0500

    prelab 5 complete

--- Prelabs/prelab5/prelab5.c
+++ Prelabs/prelab5/prelab5.c
@@ -1,5 +1,5 @@
-#include <stdlib.c>
-#include <stdio.c>
+#include <stdlib.h>
+#include <stdio.h>
 
 typedef struct { 
      int empID, jobType; 
@@ -8,15 +8,19 @@ typedef struct {
 
 Employee * readEmployeeArray(FILE *fp) {
     int size, i=0;
-     fscanf(fp, "%i", length);
+    fscanf(fp, "%i", &size);
 
     Employee *list = malloc(sizeof(int) + size*sizeof(Employee));
+
+    if(!list) return NULL;
+
     *((int*) list) = size;
-    list = ((*int) list) + 1;
+    list = (Employee*) (((int*) list) + 1);
 
     while(!feof(fp)){
-        Employee guy= list[i];
-        fscanf(fptr, "%i %i %f", guy.empID, guy.jobType, guy.salary);
+        //fscanf(fp, "%i,%i,%f", &(id), &(job), &(salary));
+        fscanf(fp,"%i %i %f", &(list[i].empID), &(list[i].jobType), &(list[i].salary));
+        printf("\n%i %i %.2f", list[i].empID, list[i].jobType, list[i].salary);
         i++;
     }
 
@@ -24,15 +28,29 @@ Employee * readEmployeeArray(FILE *fp) {
 }
 
 Employee * getEmployeeByID(Employee *list, int empID){
-    return list[empID];
+    return &(list[empID-1]);
 }
 
-int setEmpSalary(Employee *, int empID, float salary){
+int setEmpSalary(Employee *list, int empID, float salary){
+    if(salary<0) return -1;
     
+    getEmployeeByID(list, empID)->salary = salary;
+    return 0;
 }
-int getEmpSalary(Employee *, int empID, float *salary)  
-int setEmpJobType(Employee *, int empID, int job) 
-int getEmpJobType(Employee *, int empID, int *job)
+int getEmpSalary(Employee *list, int empID, float *salary)  {
+    *salary = getEmployeeByID(list, empID)->salary;
+    return  0;
+}
+int setEmpJobType(Employee *list, int empID, int job){
+    if(job<0) return -1;
+    
+    getEmployeeByID(list, empID)->jobType = job;
+    return 0;
+}
+int getEmpJobType(Employee *list, int empID, int *job){
+    *job = getEmployeeByID(list, empID)->jobType;
+    return 0;
+}
 
 int getSize(void * array){
     return *(((int*)array)-1);
@@ -45,4 +63,15 @@ void freeArray(void * array){
 int main(){
     FILE *fptr = fopen("data.txt", "r");
     Employee *list = readEmployeeArray(fptr);
+    fclose(fptr);
+    float s1;
+    int j2;
+    setEmpJobType(list, 2, 99);
+    getEmpSalary(list, 1, &s1);
+    getEmpJobType(list, 2, &j2);
+
+    printf("\n\n%i", j2);
+    printf("\n%i\n", getSize(list));
+
+    freeArray(list);
 }
